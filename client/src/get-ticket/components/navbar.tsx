7c1e6ee05ad6a7fcd923428e2bf3e33a078d0ee5
SaraPoiatti <s309616@studenti.polito.it>
2024-10-12 13:06:55 +0200

    navbar + button animation

---
+++ client/src/get-ticket/components/navbar.tsx
@@ -0,0 +1,59 @@
+import { Navbar, Container } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import { MdOutlineLocalPostOffice } from "react-icons/md";
+import { IoIosInformationCircleOutline } from "react-icons/io";
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+import '../get-ticket.css';
+import { useNavigate } from 'react-router-dom';
+
+function CustomNavbar() {
+
+  const renderTooltip = (props: any) => (
+      <Tooltip className="info-text" id="button-tooltip" {...props}>
+        Hey! Select any service from<br/>
+        below that you want to avail<br/>
+        to get a ticket. If you need <br/>
+        further help, call the staff.
+      </Tooltip>
+    );
+
+  const navigate = useNavigate();
+  const goToHome = () => {
+    navigate('/');  // Navigate to the Home page
+  };
+  
+  return (
+    <Navbar className="navbar" bg="light" expand="lg">
+      <Container fluid className="navbar-container">
+        {/* Left: Back Button */}
+        <Button className="btn btn-outline" onClick={goToHome}>
+          Home
+        </Button>
+
+        {/* Center: Brand */}
+        
+        <Navbar.Brand className="post-office">
+            <MdOutlineLocalPostOffice size={60} className="post-office-icon"/>
+            <p className="post-office-title">Italian Post Office</p>
+        </Navbar.Brand>
+
+        {/* Right: Info Icon */}
+        <OverlayTrigger
+                placement="bottom-start"
+                delay={{ show: 250, hide: 400 }}
+                overlay={renderTooltip}
+                >
+                <Button className='info-btn'>
+                    <IoIosInformationCircleOutline size={28}/>
+                </Button>
+            </OverlayTrigger>
+      </Container>
+    </Navbar>
+  )
+
+
+}
+
+export default CustomNavbar
+
--- client/src/get-ticket/components/ticket.tsx
+++ client/src/get-ticket/components/ticket.tsx
@@ -1,10 +1,5 @@
 import { useState, useRef } from 'react'
-import Button from 'react-bootstrap/Button';
-import { MdOutlineLocalPostOffice } from "react-icons/md";
-import { IoIosInformationCircleOutline } from "react-icons/io";
 import myIcon from '../../assets/Down_Arrow.svg';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Tooltip from 'react-bootstrap/Tooltip';
 import '../get-ticket.css';
 
 
@@ -15,48 +10,8 @@ function Ticket() {
     const [serviceType, setServiceType] = useState("Service1");
     const [show, setShow] = useState(false);
 
-    const target = useRef(null);
-
-    const renderTooltip = (props: any) => (
-        <Tooltip className="info-text" id="button-tooltip" {...props}>
-          Hey! Select any service from<br/>
-          below that you want to avail<br/>
-          to get a ticket. If you need <br/>
-          further help, call the staff.
-        </Tooltip>
-      );
-
-
     return (
-        <>
-            {/* Back Button */}
-            <div className='back-button'>
-                <Button className="btn btn-outline">Back</Button>
-            </div>
-
-            {/* Italian Post Office */}
-            <div className='post-office'>
-                <span className='post-office-icon'>
-                    <MdOutlineLocalPostOffice size={60} />
-                </span>  
-                <span className='post-office-title'> 
-                    Italian Post Office
-                </span>              
-            </div>
-
-            {/* Info Button */}
-            <div className='info'>
-            <OverlayTrigger
-                placement="bottom-start"
-                delay={{ show: 250, hide: 400 }}
-                overlay={renderTooltip}
-                >
-                <Button className='info-btn'>
-                    <IoIosInformationCircleOutline size={28}/>
-                </Button>
-            </OverlayTrigger>
-            </div>
-            
+        <>            
             {/* Ticket Details */}
             <div className='ticket-details'>
                 <span>
