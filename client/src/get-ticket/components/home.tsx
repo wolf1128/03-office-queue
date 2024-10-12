cf08f184dfb5b93ebbed05cc86950fe9daf0bdfa
Salar Farahmand <>
2024-10-12 18:38:38 +0200

    Added document assets | Added mockups

--- client/src/get-ticket/components/home.tsx
+++ client/src/get-ticket/components/home.tsx
@@ -1,40 +1,51 @@
-import { useState } from 'react'
-import Button from 'react-bootstrap/Button';
-import '../get-ticket.css';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import "../get-ticket.css";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
+  const goToTicket = () => {
+    navigate("/ticket"); // Navigate to the Home page
+  };
 
-    const navigate = useNavigate();
-    const goToTicket = () => {
-      navigate('/ticket');  // Navigate to the Home page
-    };
+  return (
+    <>
+      {/* Ticket Details */}
+      <div className="ticket-details">
+        <span>Select a Service</span>
+      </div>
 
-    return (
-        <>
-            {/* Ticket Details */}
-            <div className='ticket-details'>
-                <span>
-                    Select a Service
-                </span>
-            </div>
+      {/* Button Containers */}
+      <div className="button-container">
+        <Button className="service-btn" onClick={goToTicket}>
+          Service 01
+        </Button>
+        <Button className="service-btn">Service 02</Button>
+        <Button className="service-btn">Service 04</Button>
+        <Button className="service-btn">Service 03</Button>
+        <Button className="service-btn">Service 03</Button>
 
-            {/* Button Containers */}
-            <div className='button-container'>
-                <Button className='service-btn' onClick={goToTicket}>Service 01</Button>
-                <Button className='service-btn'>Service 02</Button>
-                <Button className='service-btn'>Service 04</Button>
-                <Button className='service-btn'>Service 03</Button>
-            </div>
+        <Button className="service-btn">Service 03</Button>
 
-            {/* Help Button */}
-            <div className='help-box'>
-                <Button className='help-btn'>Need Help?</Button>
-            </div>
+        <Button className="service-btn">Service 03</Button>
 
+        <Button className="service-btn">Service 03</Button>
 
-        </>
-    )
+        <Button className="service-btn">Service 03</Button>
+
+        <Button className="service-btn">Service 03</Button>
+
+        <Button className="service-btn">Service 03</Button>
+
+      </div>
+
+      {/* Help Button */}
+      <div className="help-box">
+        <Button className="help-btn">Need Help?</Button>
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
