5b01d812d2bc3b74f1910c75a70a2813f426d505
SaraPoiatti <s309616@studenti.polito.it>
2024-10-15 23:47:06 +0200

    change style name

--- client/src/get-ticket/components/home.tsx
+++ client/src/get-ticket/components/home.tsx
@@ -21,28 +21,28 @@ function Home(props: Props) {
 
     return (
         <>
-            {/* Ticket Details */}
-            <div className="custom-ticket-details">
-                <span>
-                    Select a Service
-                </span>
+            <div className="custom-homepage">
+                {/* Ticket Details */}
+                <div className="custom-ticket-details">
+                    <span>
+                        Select a Service
+                    </span>
+                </div>
+
+                {/* Button Containers */}
+                <div className="custom-button-container">
+                    {props.services.map( (service: Service) => (
+                        <Button key={service.ServiceID} className="custom-service-btn" onClick={() => goToTicket(service.ServiceID)}>
+                            {service.ServiceName}
+                        </Button>
+                    ))}
+                </div>
+
+                {/* Help Button */}
+                <div className="custom-help-box">
+                    <Button className="custom-help-btn">Need Help?</Button>
+                </div>
             </div>
-
-            {/* Button Containers */}
-            <div className="button-container">
-                {props.services.map( (service: Service) => (
-                    <Button key={service.ServiceID} className="service-btn" onClick={() => goToTicket(service.ServiceID)}>
-                        {service.ServiceName}
-                    </Button>
-                ))}
-            </div>
-
-            {/* Help Button */}
-            <div className="help-box">
-                <Button className="help-btn">Need Help?</Button>
-            </div>
-
-
         </>
     )
 }
--- client/src/get-ticket/components/ticket.tsx
+++ client/src/get-ticket/components/ticket.tsx
@@ -11,47 +11,49 @@ function Ticket(props: Props) {
     );
 
     return (
-        <>            
-            {/* Ticket Details */}
-            <div className="ticket-details">
-                <span>
-                    Ticket Details
-                </span>
-            </div>
-
-            {/* Ticket Number Box */}
-            <div className="ticket-box">
-                <span className="ticket-text">
-                    Your Ticket Number
-                </span>
-                <br/>
-                <span className="ticket-number">
-                    {props.ticket.ticketID}
-                </span>
-                
-            </div>
-            
-            {/* Information Box */}
-            <div className="information-box">
-                <div className="waiting-time">
-                    Waiting Time
-                    <div className="waiting-time-number">{props.ticket.estimatedTime}</div>
+        <> 
+            <div className="custom-homepage">           
+                {/* Ticket Details */}
+                <div className="ticket-details">
+                    <span>
+                        Ticket Details
+                    </span>
                 </div>
-                <div className="service-type">
-                    Service Type
-                    <div className="service-type-number">
-                        {myService ? myService.ServiceName : "Unknown Service"}
+
+                {/* Ticket Number Box */}
+                <div className="ticket-box">
+                    <span className="ticket-text">
+                        Your Ticket Number
+                    </span>
+                    <br/>
+                    <span className="ticket-number">
+                        {props.ticket.ticketID}
+                    </span>
+                    
+                </div>
+                
+                {/* Information Box */}
+                <div className="information-box">
+                    <div className="waiting-time">
+                        Waiting Time
+                        <div className="waiting-time-number">{props.ticket.estimatedTime}</div>
+                    </div>
+                    <div className="service-type">
+                        Service Type
+                        <div className="service-type-number">
+                            {myService ? myService.ServiceName : "Unknown Service"}
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            {/* Get Your Receipt */}
-            <div className="get-receipt">
-                <span className="get-receipt-text">
-                    Get your Receipt
-                </span>
-                <br/>    
-                <img src={myIcon} alt="My Local Icon" width="60" height="60" />
+                {/* Get Your Receipt */}
+                <div className="get-receipt">
+                    <span className="get-receipt-text">
+                        Get your Receipt
+                    </span>
+                    <br/>    
+                    <img src={myIcon} alt="My Local Icon" width="60" height="60" />
+                </div>
             </div>
         </>
     )
