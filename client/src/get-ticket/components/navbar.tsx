db9ae490a0a6bffc262bfb9d7f39ebccb96197cf
Matteo Petrera s331356 <s331356@studenti.polito.it>
2024-10-15 19:31:17 +0200

    resolved some conflicts with bootstrap css

--- client/src/get-ticket/components/navbar.tsx
+++ client/src/get-ticket/components/navbar.tsx
@@ -1,31 +1,31 @@
-import { Navbar, Container } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+import { Navbar, Container } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import { MdOutlineLocalPostOffice } from "react-icons/md";
 import { IoIosInformationCircleOutline } from "react-icons/io";
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Tooltip from 'react-bootstrap/Tooltip';
-import Button from 'react-bootstrap/Button';
-import '../get-ticket.css';
-
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
+import Button from "react-bootstrap/Button";
+import "../get-ticket.css";
 
 function CustomNavbar() {
-
   const renderTooltip = (props: any) => (
-      <Tooltip className="info-text" id="button-tooltip" {...props}>
-        Hey! Select any service from<br/>
-        below that you want to avail<br/>
-        to get a ticket. If you need <br/>
-        further help, call the staff.
-      </Tooltip>
-    );
+    <Tooltip className="info-text" id="button-tooltip" {...props}>
+      Hey! Select any service from
+      <br />
+      below that you want to avail
+      <br />
+      to get a ticket. If you need <br />
+      further help, call the staff.
+    </Tooltip>
+  );
 
   const navigate = useNavigate();
   const goToHome = () => {
-    navigate('/');  // Navigate to the Home page
+    navigate("/"); // Navigate to the Home page
   };
-  
+
   return (
-    <Navbar className="navbar" bg="light" expand="lg">
+    <Navbar className="navbar" expand="lg">
       <Container fluid className="navbar-container">
         {/* Left: Back Button */}
         <Button className="btn btn-outline" onClick={goToHome}>
@@ -34,26 +34,23 @@ function CustomNavbar() {
 
         {/* Center: Brand */}
         <Navbar.Brand className="post-office">
-            <MdOutlineLocalPostOffice size={60} className="post-office-icon"/>
-            <p className="post-office-title">Italian Post Office</p>
+          <MdOutlineLocalPostOffice size={60} className="post-office-icon" />
+          <p className="post-office-title">Italian Post Office</p>
         </Navbar.Brand>
 
         {/* Right: Info Icon */}
         <OverlayTrigger
-                placement="bottom-start"
-                delay={{ show: 250, hide: 400 }}
-                overlay={renderTooltip}
-                >
-                <Button className='info-btn'>
-                    <IoIosInformationCircleOutline size={28}/>
-                </Button>
-            </OverlayTrigger>
+          placement="bottom-start"
+          delay={{ show: 250, hide: 400 }}
+          overlay={renderTooltip}
+        >
+          <Button className="info-btn">
+            <IoIosInformationCircleOutline size={28} />
+          </Button>
+        </OverlayTrigger>
       </Container>
     </Navbar>
-  )
-
-
+  );
 }
 
-export default CustomNavbar
-
+export default CustomNavbar;
