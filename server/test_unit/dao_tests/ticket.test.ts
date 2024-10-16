c741efc841b8185ae383a8d719dfdad37a7e6e80
Simone Marcos Sagaria <simon17122001@gmail.com>
2024-10-16 14:47:57 +0200

    First unit tests done

--- server/test_unit/dao_tests/ticket.test.ts
+++ server/test_unit/dao_tests/ticket.test.ts
@@ -11,7 +11,7 @@ const date = new Date();
 const formattedDate = `${date.getFullYear()}-${String(
   date.getMonth() + 1
 ).padStart(2, "0")}-${String(date.getDate()).padStart(2, "0")}`;
-let testNewTicket = new Ticket(1, 1, formattedDate, 0, "in queue");
+let testNewTicket = new Ticket(1, 1, formattedDate, 0, "in queue",0);
 let testService = new Service(1, "Shipping", 10);
 
 describe("TicketDAO unit tests", () => {
@@ -38,6 +38,105 @@ describe("TicketDAO unit tests", () => {
 
       expect(result).toEqual(testNewTicket);
       mockDBRun.mockRestore();
+      mockDBGet.mockRestore();
+    });
+  });
+
+  describe("nextCustomer", () => {
+    test("It should resolve the next customer ticket", async () => {
+      const ticketDAO = new TicketDAO();
+
+      const expectedTicket= new Ticket(1,1,"2024-10-12",0,"in progress",1)
+
+      const mockDBGet = jest
+        .spyOn(db, "get")
+        .mockImplementation((sql, [], callback) => {
+          const mockRow: any = { 
+            TicketID:1,
+            ServiceID:1,
+            IssuedTime:"2024-10-12",
+            EstimatedTime:0
+          };
+          callback(null, mockRow);
+          return {} as Database;
+        });
+
+      const mockDBRun = jest
+        .spyOn(db, "run")
+        .mockImplementation((sql, [], callback) => {
+          callback(null);
+          return {} as Database;
+        });
+
+      const result = await ticketDAO.nextCustomer(1);
+
+      expect(result).toEqual(expectedTicket);
+      mockDBRun.mockRestore();
+      mockDBGet.mockRestore();
+    });
+  });
+
+  describe("getQueues", () => {
+    test("It should resolve the queues array", async () => {
+      const ticketDAO = new TicketDAO();
+
+      const expectedQueues=[
+        {ServiceName:"Service A",length:2},
+        {ServiceName:"Service B",length:0}
+      ]
+
+      const mockDBAll = jest
+        .spyOn(db, "all")
+        .mockImplementation((sql, [], callback) => {
+          const mockRows: any = [
+            {ServiceName:"Service A",length:2},
+            {ServiceName:"Service B",length:0}
+          ];
+          callback(null, mockRows);
+          return {} as Database;
+        });
+
+      const result = await ticketDAO.getQueues();
+
+      expect(result).toEqual(expectedQueues);
+      mockDBAll.mockRestore();
+    });
+  });
+
+  describe("getMyTicket", () => {
+    test("It should resolve the queues array", async () => {
+      const ticketDAO = new TicketDAO();
+
+      const mockDBGet = jest
+        .spyOn(db, "get")
+        .mockImplementation((sql, [], callback) => {
+          const mockRow: any = testNewTicket;
+          callback(null, mockRow);
+          return {} as Database;
+        });
+
+      const result = await ticketDAO.getMyTicket(1);
+
+      expect(result).toEqual(testNewTicket);
+      mockDBGet.mockRestore();
+    });
+  });
+
+  describe("getNextTicket", () => {
+    test("It should resolve the queues array", async () => {
+      const ticketDAO = new TicketDAO();
+
+      const mockDBGet = jest
+        .spyOn(db, "get")
+        .mockImplementation((sql, [], callback) => {
+          const mockRow: any = testNewTicket;
+          callback(null, mockRow);
+          return {} as Database;
+        });
+
+      const result = await ticketDAO.getNextTicket();
+
+      expect(result).toEqual(testNewTicket);
       mockDBGet.mockRestore();
     });
   });
